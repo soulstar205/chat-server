6ea30ca5e922db052512a58a580aff889f72a6a8
soulstar205 <soulstar205@gmail.com>
2023-10-31 23:00:56 +0100

    disabling cors

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
 require("dotenv").config();
-
 const express = require('express');
 const app = express();
 const cors = require("cors");
-// const allowCors = require('./allowCors')
-const fetch = require('node-fetch'); // In a Node.js environment, you can use 'node-fetch' for Fetch API functionality
+const fetch = require('node-fetch');
 
 app.use(express.json());
-
-// app.use(allowCors)
-app.use(cors({ origin: "https://chat-app-wine-one.vercel.app" }));
-// app.use(cors({
-//   origin: ["https://chat-app-wine-one.vercel.app/", "*"],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   methods: ["GET", "PUT", "POST"]
-// }))
+app.use(cors());
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-
   try {
     const response = await fetch("https://api.chatengine.io/users/", {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         'private-key': process.env.CHAT_ENGINE_PRIVATE_KEY,
-        "Access-Control-Allow-Origin": "https://chat-app-wine-one.vercel.app"
       },
       body: JSON.stringify({ username: username, secret: username, firstname: username }),
     });
-
     if (!response.ok) {
       throw new Error('Request failed with status: ' + response.status);
     }
-
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
 app.post("/signup", async (req, res) => {
   const { username, secret, email, first_name, last_name } = req.body;
-
-  // Store a user-copy on Chat Engine!
   try {
     const response = await fetch("https://api.chatengine.io/users/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Private-Key": process.env.CHAT_ENGINE_PRIVATE_KEY,
-        "Access-Control-Allow-Origin": "https://chat-app-wine-one.vercel.app"
       },
       body: JSON.stringify({ username, secret, email, first_name, last_name }),
     });
-
     if (response.ok) {
       const data = await response.json();
       return res.status(response.status).json(data);
@@ -69,8 +53,6 @@ app.post("/signup", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, secret } = req.body;
-
-  // Fetch this user from Chat Engine in this project!
   try {
     const response = await fetch("https://api.chatengine.io/users/me/", {
       headers: {
@@ -79,7 +61,6 @@ app.post("/login", async (req, res) => {
         "User-Secret": secret,
       },
     });
-
     if (response.ok) {
       const data = await response.json();
       return res.status(response.status).json(data);
@@ -91,7 +72,6 @@ app.post("/login", async (req, res) => {
     return res.status(500).json({ error: e.message });
   }
 });
-
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
